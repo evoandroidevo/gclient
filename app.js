2f0c6230cd5baf8fb1fde67b2ac5fddc068eee1c
evoandroidevo <3161357+evoandroidevo@users.noreply.github.com>
2021-12-15 16:41:54 -0600

    removed auto login of abc

--- app.js
+++ app.js
@@ -65,8 +65,6 @@ baserouter.get("/", function (req, res) {
           "settings":{
             "hostname":"127.0.0.1",
             "port":"3389",
-            "username":"abc",
-            "password":"abc",
             "security": "any",
             "ignore-cert": true
           }
